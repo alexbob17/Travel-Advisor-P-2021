7c10be0676daa84ba37000edc21261a288588ae3
alexbob17 <57636640+alexbob17@users.noreply.github.com>
2021-08-30 20:22:52 -0600

    material ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
+// Importando la liberia de css material core ui
+import { CssBaseline, Grid } from '@material-ui/core';
+
+
+// Importando los componentes de la App
+import Header from './components/Header/Header';
+import List from './components/List/List';
+import Map from './components/Map/Map';
+import PlaceDetails from './components/PlaceDetails/PlaceDetails';
+
+
+
 
 const App = () => {
-
     return (
-        <div>
-            <h1>Hello World</h1>
-         </div>   
+        <>
+            <CssBaseline />
+            <Header></Header>
+            <Grid container spacing={3} style ={{ width: '100%'}}>
+                <Grid item xs={12}  md={4}>
+                    <List></List>
+                </Grid>
+                <Grid item xs={12}  md={8}>
+                    <Map></Map>
+                </Grid>
+            </Grid>
+         </>   
     );
 }
 
